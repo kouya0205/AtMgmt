c865f999149ec9a45e048c8869570185dc605852
kouya0205 <kouya0205game@gmail.com>
2023-10-18 12:08:55 +0900

    Clock

--- src/App.js
+++ src/App.js
@@ -1,27 +1,23 @@
 import './App.css';
 import ComeBtn from './compounents/comeBtn';
 import OutBtn from './compounents/outBtn';
-import Clock from './compounents/clock'
+import Clock from './compounents/clock';
 
 function App() {
   return (
     <>
       <div className='centering'>
           <h1>退勤管理サイト</h1>
-          <div>
-            <input type="tel" className='nameBar'/>
-            <button className='checkBtn'>確定</button>
-          </div>
+        <div>
+          <input type="tel" className='nameBar'/>
+          <button className='checkBtn'>確定</button>
+        </div>
         <div class="container">
         <Clock />
-        <div class="clock">
-            <p class="clock-date"></p>
-            <p class="clock-time"></p>
-          </div>
         </div>
         <div className='Btn'>
-          <ComeBtn />
-          <OutBtn />
+          {/* <ComeBtn />
+          <OutBtn /> */}
         </div>
       </div>
     </>
