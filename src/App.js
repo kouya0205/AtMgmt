6cc0cbd8533f68caebf9a030047663884793fa41
kouya0205 <kouya0205game@gmail.com>
2023-10-18 16:06:35 +0900

    react-router-dom

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import ComeBtn from './compounents/comeBtn';
 import OutBtn from './compounents/outBtn';
 import Clock from './compounents/clock';
+import { Link } from 'react-router-dom';
 
 function App() {
   return (
@@ -10,14 +11,14 @@ function App() {
           <h1>退勤管理サイト</h1>
         <div>
           <input type="tel" className='nameBar'/>
-          <button className='checkBtn'>確定</button>
+          <button className='checkBtn' >PUSH!<i class="fas fa-angle-right fa-position-right"></i></button>
         </div>
         <div class="container">
         <Clock />
         </div>
         <div className='Btn'>
-          {/* <ComeBtn />
-          <OutBtn /> */}
+          <ComeBtn />
+          <OutBtn />
         </div>
       </div>
     </>
