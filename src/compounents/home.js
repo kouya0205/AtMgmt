ced06e0d3a1111ec64bc3b53d631ce00b9b484f9
kouya0205 <kouya0205game@gmail.com>
2023-11-04 19:19:13 +0900

    [fix] babel/plugin
    依存関係の改善、更新

--- src/compounents/home.js
+++ src/compounents/home.js
@@ -5,7 +5,8 @@ import { useAuthState} from 'react-firebase-hooks/auth';
 import ComeBtn from './comeBtn';
 import OutBtn from './outBtn';
 import UserInfo from './userInfo';
-
+import { IoLogOutOutline } from "react-icons/io5";
+import { FcGoogle } from 'react-icons/fc';
 
 
 
@@ -37,9 +38,10 @@ function SignInBtn() {
     };
 
     return (
-        <button onClick={signInGoogle} className='bg-[#dd4b39] btn-sns mt-7'>
-            Googleでログイン
-        </button>
+        <button onClick={signInGoogle} className='bg-[#dd4b39] btn-sns mt-7 flex items-center '>
+            <FcGoogle className=' ml-4 text-2xl'/>
+            <p className=' ml-4'>Googleでログイン</p>
+        </button> 
     )
 }
 function SignOutBtn() {
@@ -47,6 +49,7 @@ function SignOutBtn() {
     return (
         <button onClick={() => auth.signOut()} className='btn-sns mt-7 w-[170px] bg-[#5b75b8] '>
         <div className=' flex items-center text-center'>
+        <IoLogOutOutline className='text-2xl'/>
         <p>ログアウトして戻る</p>
         </div>
         </button>
