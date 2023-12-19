c9067048461639473adb7477b9c77b7b73d5e92f
kouya0205 <kouya0205game@gmail.com>
2023-12-19 20:59:08 +0900

    [fix] sidebar change responsive

--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -4,14 +4,22 @@ import UserInfo from './userInfo';
 import SignOutBtn from './btn/signOutBtn';
 
 function Sidebar() {    
+    const [sidebar, setSidebar] = React.useState(false);
+    const showSidebar = () => setSidebar(!sidebar);
+
     return (
-        <>
-        <div className=' h-[100%] w-[250px] bg-[#466a91] flex justify-center flex-col'>
+        <> 
+        <div className=' ml-1 mt-1 hidden cursor-pointer max-md:block ' onClick={showSidebar}>
+            <div className=' w-6 h-1 bg-[#466a91] m-1'></div>
+            <div className=' w-6 h-1 bg-[#466a91] m-1'></div>
+        </div>
+        <div className=' h-[100%] w-[250px] bg-[#466a91] flex justify-center flex-col max-md:opacity-0 max-md:absolute max-md:translate-y-[100] overflow-x-hidden'>
+        
         <UserInfo />
-            <ul className=' h-auto p-0 w-[100%]'>
+            <ul className=' h-auto p-0 w-[100%] '>
                 {SidebarData.map((value, key) => { 
                     return (
-                    <li key={key} className="" onClick={() => {
+                    <li key={key} className=" " onClick={() => {
                     window.location.pathname = value.link;
                     }}>
                     <div id='title' className=' w-[100%] h-[100%] items-center text-white text-center mt-4 mb-4'>{value.title}</div>
@@ -20,7 +28,10 @@ function Sidebar() {
                 })}
             </ul>
             <SignOutBtn />
+            
         </div>
+        
+        
         </>
         
     )
