0b16b99f95b29ee3f27bced23585c99ed0676c40
kouya0205 <kouya0205game@gmail.com>
2024-01-28 12:19:40 +0900

    [fix] other user state && fix grobal state

--- src/components/myPage.js
+++ src/components/myPage.js
@@ -3,18 +3,26 @@ import Headers from './header';
 import List from './list';
 import AtTab from './tab';
 import Navbar from './Navbar.jsx';
-import { collection, onSnapshot } from 'firebase/firestore';
+import { doc, onSnapshot } from 'firebase/firestore';
 import { db } from '../firebase';
+import { getUserId } from './dbFn';
 
 const MyPage = () => {
   const [Time, setTime] = useState([]);
   useEffect(() => {
-    const getUserData = collection(db, 'users');
-    //リアルタイムデータを取得
-    onSnapshot(getUserData, (querySnapshot) => {
-      console.log(querySnapshot.docs.map((doc) => doc.data()));
-      setTime(querySnapshot.docs.map((doc) => doc.data()));
-    });
+    const fetchData = async () => {
+      const userId = await getUserId();
+      const userDocRef = doc(db, 'users', userId);
+      onSnapshot(userDocRef, (doc) => {
+        if (doc.exists()) {
+          console.log(doc.data());
+          setTime([doc.data()]);
+        } else {
+          console.log('No such document!');
+        }
+      });
+    };
+    fetchData();
   }, []);
 
   return (
@@ -22,15 +30,16 @@ const MyPage = () => {
       <div className=" flex flex-col min-[769px]:flex-row-reverse ">
         <div className="">
           <Headers />
-          {Time.map((time) => (
-            <List
-              key={time.id}
-              comeInTime={time.comeIn}
-              comeOutTime={time.comeOut}
-              breakStrTime={time.breakStr}
-              breakFinTime={time.breakFin}
-            />
-          ))}
+          {Array.isArray(Time) &&
+            Time.map((time) => (
+              <List
+                key={time.id}
+                comeInTime={time.comeIn}
+                comeOutTime={time.comeOut}
+                breakStrTime={time.breakStr}
+                breakFinTime={time.breakFin}
+              />
+            ))}
           <AtTab />
         </div>
         <Navbar />
