3da160729ab4526a68438b4cf6f71bd51552db08
kouya0205 <kouya0205game@gmail.com>
2024-02-06 21:33:56 +0900

    [fix] more collection data use orderBy

--- src/components/myPage.js
+++ src/components/myPage.js
@@ -3,24 +3,47 @@ import Headers from './header';
 import List from './list';
 import AtTab from './tab';
 import Navbar from './Navbar.jsx';
-import { doc, onSnapshot } from 'firebase/firestore';
+import {
+  doc,
+  collection,
+  onSnapshot,
+  query,
+  orderBy,
+  limit,
+  getDocs,
+} from 'firebase/firestore';
 import { db } from '../firebase';
 import { getUserId } from './dbFn';
 
 const MyPage = () => {
+  //画面レンダリング時にデータを取得する
   const [Time, setTime] = useState([]);
   useEffect(() => {
     const fetchData = async () => {
       const userId = await getUserId();
       const userDocRef = doc(db, 'users', userId);
-      onSnapshot(userDocRef, (doc) => {
-        if (doc.exists()) {
-          console.log(doc.data());
-          setTime([doc.data()]);
-        } else {
-          console.log('No such document!');
-        }
+      const dateCollection = collection(userDocRef, 'dates');
+
+      const q = query(dateCollection, orderBy('Timestamp', 'desc'), limit(1));
+      const querySnapshot = await getDocs(q);
+
+      let latestDocId = '';
+      querySnapshot.forEach((doc) => {
+        latestDocId = doc.id;
+        console.log(latestDocId);
       });
+
+      if (latestDocId) {
+        const latestDocRef = doc(dateCollection, latestDocId);
+        onSnapshot(latestDocRef, (doc) => {
+          if (doc.exists()) {
+            console.log(doc.data());
+            setTime([doc.data()]);
+          } else {
+            console.log('No such document!');
+          }
+        });
+      }
     };
     fetchData();
   }, []);
